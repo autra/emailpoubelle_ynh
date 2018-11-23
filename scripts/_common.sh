31f584af9470928ea026cf35b7f2b8a993faea2b
Krakinou <Krakinou@users.noreply.github.com>
2018-11-24 00:08:43 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,5 @@
 #=================================================
 pkg_dependencies="php-geoip"
 app="emailpoubelle"
+lang="fr_FR.UTF-8 en_US.UTF-8 it_IT.UTF-8"
+cronline="0 */2 * * * www-data cd $final_path/www/; /usr/bin/php index.php > /dev/null 2>&1"
